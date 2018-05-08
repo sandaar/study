e26da9d111a67906acc7de89b21ae5833e1911aa
Victoria Rozhina <victoria@rescale.com>
2018-05-07 20:40:46 -0700

    fixed bubble sort

--- sorting/bubble/bubble.py
+++ sorting/bubble/bubble.py
@@ -1,47 +1,58 @@
 import unittest
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
 
 class TestBubbleSort(unittest.TestCase):
     def test_empty_array(self):
         array = []
-        result = bubble_sort(array)
-        self.assertEqual(result, array)
+        bubble_sort(array)
+        self.assertEqual(array, [])
 
     def test_one_item(self):
         array = [5]
-        result = bubble_sort(array)
-        self.assertEqual(result, array)
+        bubble_sort(array)
+        self.assertEqual(array, [5])
 
     def test_many_items(self):
         array = [16, 4, 8, 3, 0, 10]
-        result = bubble_sort(array)
-        expected_result = [0, 3, 4, 8, 10, 16]
-        self.assertEqual(result, expected_result)
+        bubble_sort(array)
+        expected = [0, 3, 4, 8, 10, 16]
+        self.assertEqual(array, expected)
 
-    def test_prebubble_sorted(self):
+    def test_presorted(self):
         array = [0, 3, 4, 8, 10, 16]
-        result = bubble_sort(array)
-        expected_result = [0, 3, 4, 8, 10, 16]
-        self.assertEqual(result, expected_result)
+        bubble_sort(array)
+        expected = [0, 3, 4, 8, 10, 16]
+        self.assertEqual(array, expected)
+
+    def test_odd(self):
+        array = [16, 4, 8, 3, 0, 10, 9]
+        bubble_sort(array)
+        expected = [0, 3, 4, 8, 9, 10, 16]
+        self.assertEqual(array, expected)
 
 
 def bubble_sort(array):
+    ''' at any given loop iteration following is true:
+            i is number of sorted elements 
+            n - 1 - i is start index for sorted part of the array 
+            j is index of an element that will be compared to right neighbor
+    '''
     n = len(array)
-    if n in [0, 1]:
-        return array
-    if n in [0, 1]:
-        return array
+    logger.debug("Start bubble sort\n%s", array)
     for i in xrange(0, n):
-        min_index = 0
-        min_item = array[0]
-        for j in xrange(0, n - i):
-            if array[j] <= min_item:
-                min_index = j
-                min_item =  array[j]
-        array.append(array[min_index])
-        del array[min_index]
-#        print "index {}, value {}".format(min_index, min_item)
-#        print array
-    return array
+        logger.debug("i = %s", i)
+        for j in xrange(0, n - 1 - i):
+            logger.debug("j = %s", j)
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+            logger.debug(array)
 
 if __name__ == '__main__':
     unittest.main()
